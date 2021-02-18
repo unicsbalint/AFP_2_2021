579d3e13070ae460fad9bfb92a645d2b788c2d5b
cserneczkybalint <cserneczkybalint@gmail.com>
2021-02-18 18:10:05 +0100

    Homepage háttér beállítása, navbar implementálása

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
-<?php
-echo "Hello world :D";
-?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="frontEnd/css/style.css">
+    <script src="frontEnd/js/dummyScript.js"></script>
+    <title>Anabolic Tesla</title>
+</head>
+<body>
+<div class="navContainer">
+    <div class="topnav">
+      <a href="#">MODEL S</a>
+      <a href="#">MODEL 3</a>
+      <a href="#">MODEL X</a>
+      <a href="#">MODEL Y</a>
+    </div>
+  </div>
+    <div id="homepage"></div>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, pariatur minus dignissimos perspiciatis animi molestiae atque placeat, est iste expedita quibusdam laborum excepturi. Placeat delectus deserunt quibusdam asperiores, natus autem.
+    Impedit, vero blanditiis sed earum necessitatibus et nostrum adipisci dolorem veniam voluptate, aliquam placeat. Dolor, nesciunt iure iusto vitae assumenda, excepturi aliquam modi quod deserunt minus voluptates, molestiae soluta veniam!
+    Ex excepturi debitis, laborum quaerat animi mollitia ullam ea ducimus? Impedit ducimus quod ea vel, sapiente asperiores quae laudantium modi ipsum mollitia, dolore hic explicabo aliquid ex ad quibusdam repellendus!
+    Deserunt deleniti recusandae iusto odit officiis soluta consequatur est pariatur repudiandae sunt autem perspiciatis, distinctio neque in ducimus obcaecati voluptate reprehenderit hic illum corporis vel quis rerum? Asperiores, animi quibusdam.
+    Iste blanditiis maxime numquam libero reiciendis mollitia ullam optio commodi molestias magni minima, autem amet praesentium et voluptates harum nihil nesciunt. Perferendis culpa, reprehenderit amet inventore consectetur recusandae optio cum?
+    Porro placeat labore a doloremque reiciendis in odit voluptas error sed vitae, aliquam tempora officia iusto fuga iure sint aliquid deleniti fugit veritatis ratione, perferendis totam blanditiis impedit nulla? Quisquam?
+    Ad non dolorem eos hic ab, deserunt consequuntur nulla, recusandae alias modi, cupiditate fugiat? Dolore, soluta odit. Libero ratione iusto error molestias nemo. Est doloremque consectetur laboriosam minima! Facere, quos!
+    Eligendi cum ut beatae laborum reprehenderit vitae? Aliquam officia doloribus ullam commodi suscipit consequuntur corrupti, aliquid aspernatur accusantium unde dolorem similique saepe sapiente? Magni, illum nostrum quos fugit vel alias.
+    Cumque iusto ad excepturi accusantium et nihil cum deserunt pariatur, autem animi minima dolor mollitia reprehenderit reiciendis at, illum nesciunt voluptatibus fugiat error hic labore, non aperiam ducimus. Non, animi?
+    Dolores nobis adipisci quod laboriosam vero aspernatur ipsum quis, ipsam saepe fugiat assumenda porro asperiores iste aliquid ut perferendis soluta deserunt rem? Quaerat possimus error, deleniti mollitia laborum totam repellendus!
+</body>
+</html>
