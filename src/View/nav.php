f4c0ce1ed6278d4d3600c93a0eb89b99ef406634
Péter Molnár <xodax9898@gmail.com>
2021-04-21 23:01:20 +0200

    Bug fixes, now the jQuerry functions are working, added some style changes

--- src/View/nav.php
+++ src/View/nav.php
@@ -1,10 +1,11 @@
-
+<div class="navContainer">
     <div class="navbar" >
-      <a class="nav-item active" id="modelS">MODEL S</a>
-      <a class="nav-item active" id="model3">MODEL 3</a>
-      <a class="nav-item active" id="modelX">MODEL X</a>
-      <a class="nav-item active" id="modelY">MODEL Y</a>
-        <a class="nav-link" href="order/order.php">Rendelés</a>
-        <a class="nav-link" href="/AFP_2_2021/src/view/">Főoldal</a>
-
+        <a class="nav-item active" id="modelS">MODEL S</a>
+        <a class="nav-item active" id="model3">MODEL 3</a>
+        <a class="nav-item active" id="modelX">MODEL X</a>
+        <a class="nav-item active" id="modelY">MODEL Y</a>
+        <a class="nav-item active" href="order/order.php">Order</a>
+        <a class="nav-item active" href="/AFP_2_2021/src/View/index.php">Home</a>
     </div>
+</div>
+
