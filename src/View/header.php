328a4ddde8a1fc238624de275be87069ff517f5b
Levi0317 <65221472+Levi0317@users.noreply.github.com>
2021-05-05 09:08:38 +0200

    cookies hivatkozás

--- src/View/header.php
+++ src/View/header.php
@@ -11,5 +11,9 @@
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" type="text/css" href="src/View/js/cookie warning/purecookie.css" async />
+    <script src="src/View/js/cookie warning/purecookie.js" async></script>
+
     <title>Anabolic Tesla</title>
 </head>
