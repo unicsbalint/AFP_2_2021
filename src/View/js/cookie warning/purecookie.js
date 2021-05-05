482d039f4520dfd5ca521f2f1644ff683e61f16f
Levi0317 <65221472+Levi0317@users.noreply.github.com>
2021-05-05 08:42:06 +0200

    cookie figyelmeztetés kidolgozása

---
+++ src/View/js/cookie warning/purecookie.js
@@ -0,0 +1,4 @@
+var purecookieTitle = "Süti figyelmeztetés";
+var purecookieDesc = "A weboldal használatával automatikusan elfogadod a sütikre való szabályzatot";
+var purecookieLink = '<a href="https://hu.wikipedia.org/wiki/HTTP-s%C3%BCti" target="_blank">Mire jók a sütik?</a>';
+var purecookieButton = "Megértettem!";
