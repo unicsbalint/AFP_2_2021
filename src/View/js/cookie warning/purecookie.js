8bfa2cb99580bf88c0d360f40df966974b004c86
Levi0317 <65221472+Levi0317@users.noreply.github.com>
2021-05-05 08:45:26 +0200

    cookies figyelmeztetés kidolgozása pureFadeIn function

--- src/View/js/cookie warning/purecookie.js
+++ src/View/js/cookie warning/purecookie.js
@@ -2,3 +2,17 @@ var purecookieTitle = "Süti figyelmeztetés";
 var purecookieDesc = "A weboldal használatával automatikusan elfogadod a sütikre való szabályzatot";
 var purecookieLink = '<a href="https://hu.wikipedia.org/wiki/HTTP-s%C3%BCti" target="_blank">Mire jók a sütik?</a>';
 var purecookieButton = "Megértettem!";
+
+function pureFadeIn(elem, display){
+  var el = document.getElementById(elem);
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .02) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+};
