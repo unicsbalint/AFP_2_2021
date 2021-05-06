9384b62faa956fcea3085e1752bb823d0be53d8d
Péter Molnár <xodax9898@gmail.com>
2021-05-06 09:56:43 +0200

    Modal felugró ablak alapvető struktúrájának kialakítása (WIP)

--- src/View/order/order.php
+++ src/View/order/order.php
@@ -9,7 +9,6 @@ $tipusok = $orderController->getTypes();
 ?>
 <body>
 <?php
-//include "../nav.php";
 ?>
 <section class="container">
     <form method="post" class="form-group" name="orderForm" >
@@ -25,33 +24,55 @@ $tipusok = $orderController->getTypes();
     </select>
 
     <label for="carmodel">Válasszon egy modellt</label><br></>
-<select name="models" id="cars" class="form-control col-12">
-    <?php
-    $selectedType = $orderController->getTypes();
-    $count = count($selectedType) - 1;
-    for ($i = 0; $i <= $count; $i++) {
-        ?>
-        <option value="<?php echo $selectedType[$i]["id"] ?>"><?php echo $selectedType[$i]["model_name"] ?>$ </option>
-    <?php } ?>
-</select>
-<label for="extras">Válasszon egy extrat</label>
-<select name="extras" id="extras" class="form-control col-12">
-    <?php
-    $selectedExtras = $orderController->getExtras();
-    $count = count($selectedType) - 1;
-    for ($i = 0; $i <= $count; $i++) {
-        ?>
-        <option value="<?php echo $selectedExtras[$i]["_id"] ?>"><?php echo $selectedExtras[$i]["description"] ?> </option>
-    <?php } ?>
-</select>
+    <select name="models" id="cars" class="form-control col-12">
+        <?php
+        $selectedType = $orderController->getTypes();
+        $count = count($selectedType) - 1;
+        for ($i = 0; $i <= $count; $i++) {
+            ?>
+            <option value="<?php echo $selectedType[$i]["id"] ?>"><?php echo $selectedType[$i]["model_name"] ?>$ </option>
+        <?php } ?>
+    </select>
+    <label for="extras">Válasszon egy extrat</label>
+    <select name="extras" id="extras" class="form-control col-12">
+        <?php
+        $selectedExtras = $orderController->getExtras();
+        $count = count($selectedType) - 1;
+        for ($i = 0; $i <= $count; $i++) {
+            ?>
+            <option value="<?php echo $selectedExtras[$i]["_id"] ?>"><?php echo $selectedExtras[$i]["description"] ?> </option>
+        <?php } ?>
+    </select>
 
+    <label for="description">Leiras</label>
+    <input type="text" name="description" class="form-control col-12">
 
-<label for="description">Leiras</label>
-<input type="text" name="description" class="form-control col-12">
-
-<input type="submit" name="submit" class="btn btn-info">
-</form>
+    <input type="submit" name="submit" class="btn btn-info">
+    </form>
 </section>
+
+    <!-- Modal ablak a submit gomb click eventjére-->
+<div class="bg-modal">
+    <div class="modal-content">
+        <form action="">
+            <div class="userDetails">
+                <label for="userDetails">Vásárló adatok</label><br>
+                <input type="text" placeholder="Név">
+                <input type="text" placeholder="E-mail">
+                <input typy="text" placeholder="Cím">
+            </div>
+
+            <div class="carDetails">
+                <label for="carDetails">Termék adatok</label><br>
+                <input type="text" placeholder="Szín">
+                <input type="text" placeholder="Model">
+                <input type="text" placeholder="Extra">
+                <input type="text" placeholder="Leírás">
+            </div>
+        </form>
+    </div>
+</div>
+
 <?php
 if (isset($_POST) && isset($_POST["submit"]) ) {
     require_once '../../Model/Car.php';
