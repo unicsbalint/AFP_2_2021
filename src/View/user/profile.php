062578d739617aa67f875dc4c158e1a7dcb9298c
cserneczkybalint <cserneczkybalint@gmail.com>
2021-05-19 21:09:33 +0200

    Profil oldal design változtatások, új háttér

--- src/View/user/profile.php
+++ src/View/user/profile.php
@@ -1,5 +1,6 @@
 <?php
-require_once "../../View/header.php";
+session_start();
+//require_once "../../View/header.php";
 require_once "../../Controller/LoginController.php";
 //require_once "../../Controller/UserController.php";
 
@@ -9,10 +10,26 @@ $lgController = LoginController::getInstance();
 
 
 ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/png" href="https://www.tesla.com/themes/custom/tesla_frontend/assets/favicons/favicon.ico">
+    <link rel="stylesheet" href="profile.css">
+
+    <title>Anabolic Tesla</title>
+</head>
 <body>
 
 <?php if ($lgController->IsUserLoggedIn()): ?>
 
+<div id="container"></div>
 <div class="container my-5">
 
 
@@ -20,7 +37,7 @@ $lgController = LoginController::getInstance();
         <div class="form-controll" id="userForm">
 
         </div>
-        <button type="button" name="submit" id="updateProf" class="btn btn-info">
+        <button type="button" name="submit" id="updateProf" class="btn btn-info">Save profile</button>
     </form>
 
     <?php  endif; ?>
